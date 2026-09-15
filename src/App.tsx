c760a47e05b4af77a015f5bb35ef29648702e60c
Flávio Gama <gama.fl88@gmail.com>
2026-09-15 17:08:26 +0100

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <main style={{ padding: '2rem', fontFamily: 'sans-serif', textAlign: 'center' }}>
+      <h1>Projeto Figma</h1>
+      <p>A aplicação foi carregada com sucesso!</p>
+    </main>
+  )
+}
